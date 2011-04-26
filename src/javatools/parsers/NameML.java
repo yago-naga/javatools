8faa583150137ff6dc37e00e7d02128ff39f52d1
smetzger <smetzger@mpi-inf.mpg.de>
2011-04-26 08:31:37 +0000

    Javatools fix: folder of config files adjusted; still hardcoded, will be adapted in the future;
    Also since I forgot to comment on the original commit:
    Revision 5953 added multilingual functionality to Name(NameML) and Dateparser(inside Dateparser) implemented partially for Spanish, German, French and Italian,
    NameML is based on config files, for now located in data/parser.
    Also added NonsharedParameter, i.e. instantiable Parameters objects.
    Furthermore PositionTracker, an object keeping track of position changes made on a string is added and functions supporting this change tracking are introduced at the parsers.

--- src/javatools/parsers/NameML.java
+++ src/javatools/parsers/NameML.java
@@ -65,7 +65,7 @@ import javatools.parsers.Char;
 
 public class NameML {
 
-  static final File CONFIG_DIR = new File("config/parser");
+  static final File CONFIG_DIR = new File("data/parser");
 
   // FIXME: this should perhaps be obtained from the configuration file
 
