86618961037ce9dea553313f18a413e625228767
edwin <edwin@mpi-inf.mpg.de>
2011-08-11 09:24:47 +0000

    added further tests

--- test/javatools/datatypes/DateParserTest.java
+++ test/javatools/datatypes/DateParserTest.java
@@ -10,19 +10,43 @@ import org.junit.Test;
 
 public class DateParserTest {
 
-	@Test
-	public void testNormalize() {
-		assertEquals("-26-##-##", DateParser.normalize("26 BC"));
+  @Test
+  public void testNormalize() {
+    assertEquals("-26-##-##", DateParser.normalize("26 BC"));
+    assertEquals("##-08-23 (53 years old)", DateParser.normalize("August 23 (53 years old)"));
+    assertEquals("{{death date and age| 1938-02-19|1877-02-01|df=y}}", DateParser.normalize("{{death date and age| 1938 |2|19|1877|2|1|df=y}}"));
 
-	}
+  }
 
-	@Test
-	public void testGetDates() {
-		List<String> dates = DateParser.getDates("the dog was born in -26-##-##");
-		assertEquals(1, dates.size());
-		assertEquals("-26-##-##", dates.get(0));
-		dates = DateParser.getDates("the dog was born in 26-##-##");
-		assertEquals(1, dates.size());
-		assertEquals("26-##-##", dates.get(0));
-	}
+  @Test
+  public void testGetDate() {
+    String[] dates = DateParser.getDate("-26-##-##");
+    assertEquals("-26", dates[0]);
+    dates = DateParser.getDate("26-##-##");
+    assertEquals("26", dates[0]);
+  }
+
+  @Test
+  public void testGetDates() {
+    List<String> dates = DateParser.getDates("the dog was born in -26-##-##");
+    assertEquals(1, dates.size());
+    assertEquals("-26-##-##", dates.get(0));
+    dates = DateParser.getDates("the dog was born in 26-##-##");
+    assertEquals(1, dates.size());
+    assertEquals("26-##-##", dates.get(0));
+    dates = DateParser.getDates("the dog was born in |26-##-##");
+    assertEquals(1, dates.size());
+    assertEquals("26-##-##", dates.get(0));
+    dates = DateParser.getDates("the dog was born in (26-##-##)");
+    assertEquals(1, dates.size());
+    assertEquals("26-##-##", dates.get(0));
+    dates = DateParser.getDates("the dog was born in )26-##-##");
+    assertEquals(1, dates.size());
+    assertEquals("26-##-##", dates.get(0));
+    dates = DateParser.getDates("{{Death date and age|1985-95-02|1913-12-10|df=y}}");
+    assertEquals(2, dates.size());
+    assertEquals("1985-##-02", dates.get(0));
+    assertEquals("1913-12-10", dates.get(1));
+  }
+
 }
