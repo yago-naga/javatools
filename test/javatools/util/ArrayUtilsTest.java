2a49391980bbd283ccb55ef2bfac6917d7771e9c
jhoffart <jhoffart@mpi-inf.mpg.de>
2011-10-13 14:30:17 +0000

    Added ArrayUtils, removed PhraseDictionary

---
+++ test/javatools/util/ArrayUtilsTest.java
@@ -0,0 +1,23 @@
+package javatools.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ArrayUtilsTest {
+
+  @Test
+  public void testIntersectArrays() {
+    
+    int[] a = new int[] { 0, 1, 2, 3 };
+    int[] b = new int[] { 3, 4, 5 };
+    int[] c = new int[] { 9 };
+    int[] d = new int[] { 1, 2, 5 };
+    
+    assertEquals(1, ArrayUtils.intersectArrays(a, b));
+    assertEquals(0, ArrayUtils.intersectArrays(a, c));
+    assertEquals(2, ArrayUtils.intersectArrays(a, d));
+    
+  }
+}
