1e5761847efcfaf37a90021c0968e3f0ff12d4c5
suchanek <suchanek@mpi-inf.mpg.de>
2012-02-10 09:44:27 +0000

    Removed compiler error by adding default methods

--- src/javatools/database/DummyDatabase.java
+++ src/javatools/database/DummyDatabase.java
@@ -1394,6 +1394,18 @@ public class DummyDatabase extends Database {
 			return null;
 		}
 
+		@Override
+		public <T> T getObject(int arg0, Class<T> arg1) throws SQLException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public <T> T getObject(String arg0, Class<T> arg1) throws SQLException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
 	}
 	
 	public static void main(String[] args) {
