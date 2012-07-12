72fb2a97c893e7d4896ef2deee646a4cd5e6b31e
smetzger <smetzger@mpi-inf.mpg.de>
2012-07-12 15:07:34 +0000

    cleaning imports

--- test/javatools/datatypes/DateParserTest.java
+++ test/javatools/datatypes/DateParserTest.java
@@ -2,8 +2,6 @@ package javatools.datatypes;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import javatools.parsers.DateParser;
